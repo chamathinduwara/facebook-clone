16e297ad4a8304b598c60a14dca875eec0428875
chamath <csschamathinduwara@gmail.com>
2023-09-12 22:07:18 +0530

    code create post component

--- src/app/_components/feed/Feed.jsx
+++ src/app/_components/feed/Feed.jsx
@@ -1,10 +1,15 @@
+import CreatePost from "../createpost/CreatePost";
+
 const Feed = () => {
-  return <div className="flex-grow h-screen pt-6 mr-6 overflow-y-auto scroll">
-    <div>
+  return (
+    <div className="flex-grow h-screen pt-6 mr-6 overflow-y-auto">
+      <div className="mx-auto max-w-md md:max-w-xl lg:max-w-2xl">
         {/* Create Post */}
-        {/* Posts */}
+        <CreatePost></CreatePost>
+        {/* pages */}
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Feed;
