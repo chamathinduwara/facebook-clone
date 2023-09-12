3c895534414279a803fcd772b06bedbb22e324c1
chamath <csschamathinduwara@gmail.com>
2023-09-12 20:24:09 +0530

    make NextAuth authentication and create pages

---
+++ src/app/_components/feed/Feed.jsx
@@ -0,0 +1,10 @@
+const Feed = () => {
+  return <div className="flex-grow h-screen pt-6 mr-6 overflow-y-auto scroll">
+    <div>
+        {/* Create Post */}
+        {/* Posts */}
+    </div>
+  </div>;
+};
+
+export default Feed;
